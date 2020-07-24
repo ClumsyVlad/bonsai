a395b08b131b7fd8b36cfb1d05945e8cfc1151ad
MAZYA <mazur097mazur@gmail.com>
2020-07-24 15:26:37 +0200

    france insurance

---
+++ src/france/sliderInit.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 100,
+    width: 310,
+    allowTouchMove: false,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'progressbar',
+    },
+    renderProgressbar: function (progressbarFillClass) {
+        return '<div class="' + progressbarFillClass + '"><p>'+ progressBar.clientWidth / 100 + '%' +'</p></div>';
+    }
+});
+const progressFill = document.querySelector('.swiper-pagination-progressbar-fill');
+const progressBar = document.querySelector('.swiper-pagination-progressbar');
+progressFill.addEventListener('click', function () {
+    this.innerText = progressBar.clientWidth / 100 + '%';
+})
