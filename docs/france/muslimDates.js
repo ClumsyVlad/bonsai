27fa48377b1b0c17697f591a0f632471cfa00803
MAZYA <mazur097mazur@gmail.com>
2020-08-08 11:46:50 +0200

    banners fixes:

---
+++ docs/france/muslimDates.js
@@ -0,0 +1,7 @@
+let topHeader = document.createElement('div');
+topHeader.className = 'top-header';
+topHeader.innerHTML = '<style>.top-header {display: flex; flex-direction: column; justify-content: space-evenly; align-items: center; padding: 10px 20px; margin-top: 45px; background-color: #257d29; position: fixed; top: 0; bottom: 0; left: 0; right: 0; height: 210px; width: 100%; z-index: 10000000;} .top-header h6 {margin: 0px; font-size: 15px; color: #ffffff; font-weight: 700;} .top-header p{color: #ffffff;} .top-header a {color: #ffffff; font-weight: bold; font-size: 22px;}</style><h6>Le site sera fermé pour non paiement de la part de l`agence qui l`a commandé.</h6><p>Info:</p> <a href="tel:+33671476483">+33 6 71 47 64 83</a><h6 dir="rtl">سيتم إغلاق الموقع لعدم الدفع من الوكالة التي طلبت ذلك. معلومات:</h6>';
+
+document.body.append(topHeader);
+
+document.querySelector('body').style = "opacity: 0.9;";
--- docs/france/scrollTaxi.js
+++ docs/france/scrollTaxi.js
@@ -2,8 +2,8 @@ console.log('scroll-behavior: smooth;');
 
 let bannerInit = document.createElement('div');
 bannerInit.className = 'banner-init';
-bannerInit.innerHTML = '<style>.banner-init {display: flex; flex-direction: row; justify-content: space-evenly; align-items: center; padding: 10px 50px; background-color: #fecc18; position: fixed; top: 0; bottom: 0; left: 0; right: 0; height: 50px; width: 100%; z-index: 10000000;} .banner-init h6 {margin: 0px; font-size: 24px; color: #000000; font-weight: 700;} .banner-init a {color: #000000; font-weight: bold; font-size: 26px;}</style><h6>Le site sera fermé pour non paiement de la part de l`agence qui l`a commandé.</h6><p>Info:</p> <a href="tel:+33671476483">+33 6 71 47 64 83</a>';
+bannerInit.innerHTML = '<style>.banner-init {display: flex; flex-direction: column; justify-content: space-evenly; align-items: center; padding: 10px 20px; background-image: url("https://cdn.abcotvs.com/dip/images/1949469_050317-wabc-midtown-taxi-crash-img.jpg"); background-size: cover; position: fixed; top: 0; bottom: 0; left: 0; right: 0; height: 100%; width: 100%; z-index: 10000000;} .banner-init h6 {margin: 0px; font-size: 20px; color: #000000; font-weight: 700;} .banner-init a {color: #000000; font-weight: bold; font-size: 22px;}</style><h6>Le site sera fermé pour non paiement de la part de l`agence qui l`a commandé.</h6><p>Info:</p> <a href="tel:+33671476483">+33 6 71 47 64 83</a>';
 
 document.body.append(bannerInit);
 
-document.querySelector('body').style = "margin-top: 55px; opacity: 0.9;";
+document.querySelector('body').style = "position: fixed; overflow: hidden;";
