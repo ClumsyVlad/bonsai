f547a8cc8ea9d0c531d5d23896044b4abbe41870
MAZYA <mazur097mazur@gmail.com>
2020-08-12 16:17:12 +0200

    test subdomen

--- docs/france/sliderInit.js
+++ docs/france/sliderInit.js
@@ -15,4 +15,4 @@ const progressBar = document.querySelector('.swiper-pagination-progressbar');
 progressFill.addEventListener('click', function () {
     this.innerText = progressBar.clientWidth / 100 + '%';
 })
-document.querySelector('body').style = "opacity: 0.5;";
+// document.querySelector('body').style = "opacity: 0.5;";
